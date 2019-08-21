4af847c6783c1527c88e658bc73267fb7b33de3f
Ryan Ersland <rersland@athenahealth.com>
2019-08-21 08:17:40 -0400

    elisp updates and organization

--- emacs-lisp/init-common.el
+++ emacs-lisp/init-common.el
@@ -147,8 +147,21 @@
 (require 'cperl-mode)
 (defalias 'perl-mode 'cperl-mode)
 
-;; major mode for LESS files
-(require 'less-css-mode)
+(setq tide-format-options '(
+  :indentSize 4
+  :tabSize 4
+  :convertTabsToSpaces nil))
+
+(defun setup-tide-mode ()
+  (interactive)
+  (tide-setup)
+  (flycheck-mode +1)
+  (setq flycheck-check-syntax-automatically '(save mode-enabled))
+  (eldoc-mode +1)
+  (tide-hl-identifier-mode +1))
+
+;; (add-hook 'before-save-hook 'tide-format-before-save)
+(add-hook 'typescript-mode-hook #'setup-tide-mode)
 
 ;; ============================================================================
 ;; key bindings
---
+++ emacs-lisp/init-wsl.el
@@ -0,0 +1,1 @@
+(define-key key-translation-map (kbd "C-M-h") (kbd "M-DEL"))
